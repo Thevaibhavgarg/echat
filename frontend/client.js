11b2d0bd6837e252a9911171826a895ad4b5f6a0
Vaibhav Garg <vaibhavgarg2003.vg@gmail.com>
2025-02-15 23:11:51 +0530

    chances the backend url

--- frontend/client.js
+++ frontend/client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:8000');
+const socket = io('https://echat-backend.onrender.com');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
